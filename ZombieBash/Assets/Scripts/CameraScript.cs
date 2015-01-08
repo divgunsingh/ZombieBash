95136ed545f52e81b0eb6552c238c4f756c64ed2
savvy <divgun.gta2@gmail.com>
2015-01-08 17:05:12 +0530

    Basic Scene Player Shooting

---
+++ ZombieBash/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	public GameObject Target;
+	public float CameraDistance=1;
+	public float CameraXDelta=1;
+	public float CameraYDelta=1;
+	public float CameraSpeed=10;
+	private float _cameraXRotation;
+	private float _cameraYRotation;
+	private float MinYRotation=-20;
+	private float MaxYRotation=90;
+
+	public void FixedUpdate(){
+		_cameraXRotation += Input.GetAxis ("Mouse X")*CameraSpeed;
+		_cameraYRotation -= Input.GetAxis ("Mouse Y")*CameraSpeed;
+
+		_cameraYRotation=ClampAngle (_cameraYRotation,MinYRotation,MaxYRotation);
+		var rotation = Quaternion.Euler(new Vector3(_cameraYRotation,_cameraXRotation,0));
+
+
+		transform.rotation=rotation;
+
+		transform.position=Target.transform.position + transform.TransformDirection(new Vector3(CameraXDelta,CameraYDelta,CameraDistance));
+
+		Target.transform.rotation=Quaternion.Euler(0,_cameraXRotation,0);
+		var player=Target.GetComponent<PlayerController>();
+		if(player!=null)
+			player.AimDirection=rotation;
+
+	}
+	private static float ClampAngle(float angle, float min,float max){
+		if (angle < -360F)
+						angle += 360F;
+		if (angle > 360F)
+						angle -= 360F;
+		return Mathf.Clamp (angle,min,max);
+
+
+	}
+}
---
+++ ZombieBash/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private bool _isGrounded;
+	public GameObject  BulletPrefab;
+	public float speed = 10;
+	public float jumpForce = 100;
+	public float  BulletForce=100;
+	public Quaternion AimDirection;
+		// Use this for initialization
+	void Start () {
+		_isGrounded = false;
+	
+	}
+	
+	void FixedUpdate() {
+		if (!_isGrounded)
+						return;
+
+		if (Input.GetButtonDown ("Fire1"))
+						shoot ();
+		if (Input.GetButton ("Fire2"))
+			shoot ();
+
+		var targetVelocity = new Vector3(Input.GetAxis ("Horizontal"),0,Input.GetAxis ("Vertical"));
+
+		rigidbody.AddForce (transform.TransformDirection(targetVelocity)*speed);
+		if (Input.GetButtonDown ("Jump")&& _isGrounded)
+						rigidbody.AddForce (new Vector3(0,jumpForce,0));
+	}
+
+	public void OnCollisionStay(){
+		Debug.Log ("On Collision Stay");
+		_isGrounded = true;
+	}
+	public void shoot(){
+		var bullet = Instantiate (BulletPrefab) as GameObject;
+
+		bullet.transform.position = transform.position;
+
+		bullet.rigidbody.velocity = (AimDirection* new Vector3(0,0,BulletForce));
+
+
+	}
+}
